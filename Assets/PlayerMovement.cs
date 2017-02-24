bb78f23a11bb9080da84477e113fbadc1cab02e3
Jordan Blakey <jordan@jordanblakey.com>
2017-02-24 00:04:55 -0600

    Initial Commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+	// Use this for initialization
+	void Start () {
+    }
+
+
+
+    // Update is called once per frame
+    // Because we're affecting physics, we use FixedUpdate() method class instead of Update()
+    void FixedUpdate () {
+
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
